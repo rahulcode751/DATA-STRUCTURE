b2cbf21dc2bdc08a6e63ad1a77c181bcec09cbd2
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-10-01 22:48:16 +0530

    Add files via upload

---
+++ searching algorithm/binary_search_.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int BinarySearch(int a[],int n,int key){
+
+         int s = 0, e = n-1;
+         while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(a[mid] == key){
+                return mid;
+            }else if(a[mid] > key){
+                e = mid-1;
+            }else{
+                s = mid + 1;
+            }
+         }
+
+         return -1;
+
+}
+
+int main(){
+
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+
+   cout<<"enter key"<<endl;
+   int key;
+   cin>>key;
+
+   sort(a,a+n);
+
+   int pos = BinarySearch(a,n,key);
+   if(pos == -1){
+    cout<<"key not found"<<endl;
+   }else{
+       cout<<"key found at index "<<pos<<endl;
+   }
+
+   return 0;
+}
+
+
+
+
---
+++ searching algorithm/linear_search_.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int a[],int n,int key){
+
+   for(int i=0;i<n;i++){
+
+    if(a[i] == key){
+        return i;
+    }
+   }
+
+   return -1;
+
+}
+
+int main(){
+
+   int n;
+   cin>>n;
+   int a[n];
+   for(int i=0;i<n;i++){
+    cin>>a[i];
+   }
+
+   cout<<"enter key"<<endl;
+   int key;
+   cin>>key;
+
+   int pos = linearSearch(a,n,key);
+   if(pos == -1){
+    cout<<"key not found"<<endl;
+   }else{
+       cout<<"key found at index "<<pos<<endl;
+   }
+
+   return 0;
+}
+
