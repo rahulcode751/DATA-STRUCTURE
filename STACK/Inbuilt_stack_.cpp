31a930cb2f97c6f87a3f6191a8e822fd62defb7a
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2021-11-03 09:16:19 +0530

    Add files via upload

---
+++ STACK/Inbuilt_stack_.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+   stack<int> s;
+   s.push(1);
+   s.push(2);
+   s.push(3);
+   s.push(4);
+   s.push(5);
+   s.push(6);
+
+   cout<<s.top()<<endl;
+   s.pop();
+   cout<<s.top()<<endl;
+   s.pop();
+
+   cout<<s.size()<<endl;
+   cout<<s.empty()<<endl;
+
+   while(!s.empty()){
+    cout<<s.top()<<endl;
+    s.pop();
+   }
+   cout<<s.size()<<endl;
+
+   return 0;
+}
+
