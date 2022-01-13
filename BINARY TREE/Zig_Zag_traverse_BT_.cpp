ce3caa84eb74749c87e0b572ce85233f3bcb6f57
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-01-13 18:07:54 +0530

    Add files via upload

---
+++ BINARY TREE/Zig_Zag_traverse_BT_.cpp
@@ -0,0 +1,25 @@
+/// qustion link -> https://practice.geeksforgeeks.org/problems/zigzag-tree-traversal/1
+vector <int> zigZagTraversal(Node* root)
+    {
+        int level = 1;
+    	vector<int> ans;
+    	queue<Node*> q;
+    	q.push(root);
+    	while(!q.empty()){
+    	    vector<int> temp;
+    	    int sz = q.size();
+    	    while(sz--){
+    	        temp.push_back(q.front()->data);
+    	        if(q.front()->left) q.push(q.front()->left);
+    	        if(q.front()->right) q.push(q.front()->right);
+    	        q.pop();
+    	    }
+    	    if(level % 2 == 0)
+    	        reverse(temp.begin(), temp.end());
+	        for(int i=0; i<temp.size(); i++)
+	            ans.push_back(temp[i]);
+            level++;
+    	}
+    	return ans;
+
+    }
