b401671085cc9982cbd6bd3c019d43ebf8b024bd
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-01-13 10:54:29 +0530

    Add files via upload

---
+++ BINARY TREE/top_view_of_BT_.c
@@ -0,0 +1,24 @@
+qustion link - > https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+vector<int> topView(Node *root)
+    {
+        vector<int> v;
+        map<int,int> m;
+        queue<pair<Node*,int>> q;
+        if(root == NULL) return v;
+        q.push({root,0});
+        while(!q.empty()){
+            Node* temp = q.front().first;
+            int h = q.front().second;
+            q.pop();
+            if(!m[h]){
+                m[h] = temp->data;
+            }
+            if(temp->left) q.push({temp->left,h-1});
+            if(temp->right) q.push({temp->right,h+1});
+        }
+
+        for(auto i:m){
+            v.push_back(i.second);
+        }
+        //Your code here
+    }
