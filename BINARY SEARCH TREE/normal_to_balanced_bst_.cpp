a57ea83bbbdba78d531d633ee72cb633852e6214
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-01-20 13:49:54 +0530

    Add files via upload

---
+++ BINARY SEARCH TREE/normal_to_balanced_bst_.cpp
@@ -0,0 +1,25 @@
+ void tree_traversal(Node*root,vector<int> &v){
+        if(root == NULL) return;
+        tree_traversal(root->left,v);
+        v.push_back(root);
+        tree_traversal(root->right,v);
+    }
+ Node* make_balanced_bst(Node*root,int s,int e,vector<Node*> &v){
+ if(s>e) return NULL;
+ int mis = (s+e)/2;
+ Node* root = v[i];
+ root->left = make_balanced_bst(root,s,mid-1,v);
+ root-right = make_balanced_bst(root,mid+1,end,v);
+ return root;
+ }
+
+ Node* normal_bst_tobalanced_bst(Node* root){
+ if(root == NULL) return NULL;
+ vector<Node*> v;
+ tree_traversal(root,v)
+ int i = 0;
+ int e = v.size() - 1;
+ root = make_balanced_bst(root,i,e,v);
+ return root;
+
+ }
