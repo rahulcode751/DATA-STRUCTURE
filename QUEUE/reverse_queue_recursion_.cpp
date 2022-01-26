66fccf5fef313f6a0e47f0a8fcf3b997f3dd18b1
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-01-26 13:56:43 +0530

    Update reverse_queue_recursion_.cpp

--- QUEUE/reverse_queue_recursion_.cpp
+++ QUEUE/reverse_queue_recursion_.cpp
@@ -1,8 +1,14 @@
-// C++ code for reversing a queue
 #include <bits/stdc++.h>
 using namespace std;
 
-// Utility function to print the queue
+void reverseQueue(queue<long long int>& q)
+{
+	if (q.empty()) return;
+	long long int data = q.front();
+	q.pop();
+	reverseQueue(q);
+	q.push(data);
+}
 void printQueue(queue<long long int> Queue)
 {
 	while (!Queue.empty()) {
@@ -11,37 +17,13 @@ void printQueue(queue<long long int> Queue)
 	}
 }
 
-// Recursive function to reverse the queue
-void reverseQueue(queue<long long int>& q)
-{
-	// Base case
-	if (q.empty())
-		return;
 
-	// Dequeue current item (from front)
-	long long int data = q.front();
-	q.pop();
-
-	// Reverse remaining queue
-	reverseQueue(q);
-
-	// Enqueue current item (to rear)
-	q.push(data);
-}
-
-// Driver code
 int main()
 {
 	queue<long long int> Queue;
 	Queue.push(56);
 	Queue.push(27);
 	Queue.push(30);
-	Queue.push(45);
-	Queue.push(85);
-	Queue.push(92);
-	Queue.push(58);
-	Queue.push(80);
-	Queue.push(90);
 	Queue.push(100);
 	reverseQueue(Queue);
 	printQueue(Queue);
