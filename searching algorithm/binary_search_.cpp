859fff97aad4c2acda6bb0714a8cc4bdc6a45e5a
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-02-01 19:18:49 +0530

    Update binary_search_.cpp

--- searching algorithm/binary_search_.cpp
+++ searching algorithm/binary_search_.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+// itrative binary search
 
 int BinarySearch(int a[],int n,int key){
-
          int s = 0, e = n-1;
          while(s<=e){
             int mid = s + (e-s)/2;
@@ -15,33 +15,26 @@ int BinarySearch(int a[],int n,int key){
                 s = mid + 1;
             }
          }
-
          return -1;
-
 }
 
 int main(){
-
    int n;
    cin>>n;
    int a[n];
    for(int i=0;i<n;i++){
     cin>>a[i];
    }
-
    cout<<"enter key"<<endl;
    int key;
    cin>>key;
-
    sort(a,a+n);
-
    int pos = BinarySearch(a,n,key);
    if(pos == -1){
     cout<<"key not found"<<endl;
    }else{
        cout<<"key found at index "<<pos<<endl;
    }
-
    return 0;
 }
 
