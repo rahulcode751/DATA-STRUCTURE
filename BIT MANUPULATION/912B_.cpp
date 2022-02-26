a15e9a3654ad3e3bd55e20e334dbac9270a295fd
RAHUL BAIRAGI <73958355+rahulcode751@users.noreply.github.com>
2022-02-26 20:42:14 +0530

    Update 912B_.cpp

--- BIT MANUPULATION/912B_.cpp
+++ BIT MANUPULATION/912B_.cpp
@@ -1,3 +1,19 @@
+/*
+Since Grisha behaved well last year, at New Year's Eve he was visited by Ded Moroz who brought an enormous bag of gifts with him! The bag contains n sweet candies from the good ol' bakery, each labeled from 1 to n corresponding to its tastiness. No two candies have the same tastiness.
+
+The choice of candies has a direct effect on Grisha's happiness. One can assume that he should take the tastiest ones — but no, the holiday magic turns things upside down. It is the xor-sum of tastinesses that matters, not the ordinary sum!
+
+A xor-sum of a sequence of integers a1, a2, ..., am is defined as the bitwise XOR of all its elements: , here  denotes the bitwise XOR operation; more about bitwise XOR can be found here.
+
+Ded Moroz warned Grisha he has more houses to visit, so Grisha can take no more than k candies from the bag. Help Grisha determine the largest xor-sum (largest xor-sum means maximum happiness!) he can obtain.
+
+Input
+The sole string contains two integers n and k (1 ≤ k ≤ n ≤ 1018).
+
+Output
+Output one number — the largest possible xor-sum.
+
+*/
 #include <bits/stdc++.h>
 using namespace std;
 bool isPowerOfTwo(int n)
@@ -12,13 +28,10 @@ int main() {
 	else if(n < k) cout<<n<<endl;
 	else{
 		int sum = 0;
-		int  i=0;
-		while(k and n>0){
-			if(isPowerOfTwo(n) == 1){
-				sum = sum xor n;
-				k--;
-			}
-			n--;
+		int  i=1;
+		while(i<=n){
+			sum = sum + i ;
+			i = i*2;
 		}
 		cout<<sum<<endl;
 	}
