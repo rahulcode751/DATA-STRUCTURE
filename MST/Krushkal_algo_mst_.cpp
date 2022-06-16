49ca68d263a01499e203ce2f51703ee9817189a5
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2022-06-16 17:10:56 +0530

    Mst is added

---
+++ MST/Krushkal_algo_mst_.cpp
@@ -0,0 +1,71 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define V 5
+int parent[V];
+
+int find(int i)
+{
+    while (parent[i] != i)
+        i = parent[i];
+    return i;
+}
+
+void union1(int i, int j)
+{
+    int a = find(i);
+    int b = find(j);
+    parent[a] = b;
+}
+
+
+void kruskalMST(int cost[][V])
+{
+    int mincost = 0; // Cost of min MST.
+    for (int i = 0; i < V; i++)
+        parent[i] = i;
+    int edge_count = 0;
+    while (edge_count < V - 1) {
+        int min = INT_MAX, a = -1, b = -1;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (find(i) != find(j) && cost[i][j] < min) {
+                    min = cost[i][j];
+                    a = i;
+                    b = j;
+                }
+            }
+        }
+
+        union1(a, b);
+        printf("Edge %d:(%d, %d) cost:%d \n",
+               edge_count++, a, b, min);
+        mincost += min;
+    }
+    printf("\n Minimum cost= %d \n", mincost);
+}
+
+
+int main()
+{
+    /* Let us create the following graph
+          2    3
+      (0)--(1)--(2)
+       |   / \   |
+      6| 8/   \5 |7
+       | /     \ |
+      (3)-------(4)
+            9          */
+    int cost[][V] = {
+        { INT_MAX, 2, INT_MAX, 6, INT_MAX },
+        { 2, INT_MAX, 3, 8, 5 },
+        { INT_MAX, 3, INT_MAX, INT_MAX, 7 },
+        { 6, 8, INT_MAX, INT_MAX, 9 },
+        { INT_MAX, 5, 7, 9, INT_MAX },
+    };
+
+    kruskalMST(cost);
+
+    return 0;
+}
